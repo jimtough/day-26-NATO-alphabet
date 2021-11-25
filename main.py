898bf99f756f380d139c15a48e6dffbcc58932b3
adultmalehuman <adultmalehuman@gmail.com>
2021-11-25 15:41:17 -0400

    refactor

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
-nato_alphabet_dict = {row['letter']:row['code'] for (index, row) in dataframe.iterrows()}
+# nato_alphabet_dict = {row['letter']:row['code'] for (index, row) in dataframe.iterrows()}
+nato_alphabet_dict = {row.letter: row.code for (index, row) in dataframe.iterrows()}
 # print(nato_alphabet_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
